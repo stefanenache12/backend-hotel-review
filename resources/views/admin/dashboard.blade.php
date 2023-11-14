f9f8702a11800126f04a24788ecb73323224332b
stefanenache12 <stefanenache12@gmail.com>
2023-11-14 17:11:12 +0100

    Add Dashboard Asside Menu

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,14 +3,5 @@
 @section('page-title', 'Dashboard')
 
 @section('main-content')
-    <div class="dashboard">
-        <div class="row gx-0">
-            <div class="asside col-2">
-                Asside
-            </div>
-            <div class="main col-10">
-                main
-            </div>
-        </div>
-    </div>
+    main
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,56 @@
         @vite('resources/js/app.js')
     </head>
     <body>
-        <main>
-            <div>
-                @yield('main-content')
+        <div class="dashboard">
+            <div class="row gx-0">
+                <div class="asside text-center col-2">
+                    <div class="lg_asside">
+                        <div class="py-4">
+                            <img class="w-75" src="/storage/img/logo1.png" alt="">
+                        </div>
+                        <div class="asside_menu py-5">
+                            <div>
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            Dashboard
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Statistics
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Sondaggi
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+            
+                                    <button type="submit" class="btn btn-outline-danger">
+                                        Log Out
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sm_asside">
+                        A
+                    </div>
+                </div>
+                <div class="main col-10">
+                    <div>
+                        <main>
+                            @yield('main-content')
+                        </main>
+                    </div>
+                </div>
             </div>
-        </main>
+        </div>
     </body>
 </html>
